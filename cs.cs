add450b00c13de367ea99e4630249ce6cc66f678
Dimitris Voutsinas <dimsvoutsinas@hotmail.com>
2023-11-23 16:46:07 +0200

    Two Equal Columns

---
+++ cs.cs
@@ -0,0 +1,31 @@
+CandidateEventItems(
+string candidateId, 
+int page, 
+string language, 
+string timeZoneId, 
+int roleId, 
+int currentLogin)
+
+
+aCandidateId = A2DDCA27334DC6A2DF2095ACDD7827AAC7BA38C9
+
+page = 1
+
+language = en
+
+userPrefs.timeZoneId = "GTB Standard Time"
+
+
+timelineRoleId = 0 (true)
+
+
+currentLogin.lId = 1092
+
+
+
+uiTerms
+
+
+
+
+
