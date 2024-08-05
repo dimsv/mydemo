f4a05425f732cec5172bb69e7b8cb72dc8e032e3
Voutsinas Dimitris <vdi@entersoft.gr>
2024-08-05 17:00:17 +0300

    updates

--- ContosoPizzaDemo/obj/Debug/net8.0/ContosoPizzaDemo.AssemblyInfo.cs
+++ ContosoPizzaDemo/obj/Debug/net8.0/ContosoPizzaDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizzaDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b8ff792117cff43891ab1698366b8125ea9d6a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b5684c08ddbad6a4a4294b19b87997b4ab2495")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizzaDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizzaDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
